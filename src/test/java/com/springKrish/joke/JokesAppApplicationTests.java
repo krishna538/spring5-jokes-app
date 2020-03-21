83da67e76faebb88d60cc68e661254e57f85eaef
krishna538 <kittu_538@yahoo.co.in>
2020-03-21 20:38:37 +0530

    first commit

---
+++ src/test/java/com/springKrish/joke/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springKrish.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
